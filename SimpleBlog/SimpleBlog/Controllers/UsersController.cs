cb77ca3cc3a3560f04cd770d6b4c8c73f6dc33b0
Vadim <32672528+Vadim23232323@users.noreply.github.com>
2021-08-18 17:46:53 +0300

    Create new controller and new page

---
+++ SimpleBlog/SimpleBlog/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using SimpleBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            UsersModel user = new UsersModel();
+            int age = 10 / user.age;
+            return View();
+        }
+
+        public ActionResult ListUsers()
+        {
+
+            List<UsersModel> users = new List<UsersModel>();
+            users.Add(new UsersModel("tom", "jack", 22, false));
+            users.Add(new UsersModel("vadim", "jack", 27, false));
+            users.Add(new UsersModel("tatia", "jack", 24, false));
+            users.Add(new UsersModel("maks", "jack", 5, false));
+            return View(users);
+        }
+    }
+}
